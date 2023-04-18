f9758303385b894aeb7e6e82174092e8d6ff8300
etienne <etienne.arnaud-goddet@etu.esisar.grenoble-inp.fr>
2023-04-18 16:41:16 +0200

    suite subbytes

--- AesFunctions.hs
+++ AesFunctions.hs
@@ -16,23 +16,12 @@ concatZ256Z (Z256Z (Poly a)) (Z256Z (Poly b)) = Z256Z (Poly (a ++ b))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 subBytes :: [Z_sur_256Z] -> [Z_sur_256Z]
-subBytes (x:xs) = subBytes_aux (inverse256 (x)) (x) (7)
+subBytes [] = []
+subBytes (x:xs) = (subBytes_aux (inverse256 (x)) (x) (7) ) : (subBytes xs)
 
 
 
 subBytes_aux :: Z_sur_256Z -> Z_sur_256Z -> Int ->Z_sur_256Z
 subBytes_aux (Z256Z (Poly [])) _ _= (Z256Z (Poly []))
-subBytes_aux (Z256Z (Poly(x:xs))) (Z256Z(Poly(y))) (i) =  concatZ256Z  (  Z256Z (Poly [( x (xor) ( ( getEl (Z256Z(Poly(y))) ( (i+4) mod (8)) ) (xor) ( ( getEl (Z256Z(Poly(y))) ((i+5) mod (8)) ) (xor) ( ( getEl ((Z256Z(Poly(y)))) ( (i+6) mod (8))) (xor) ( (getEl ((Z256Z(Poly(y)))) ((i+7) mod (8))) (xor) (getEl (Z256Z (Poly [Z2Z 0, Z2Z 1, Z2Z 1, Z2Z 0, Z2Z 0, Z2Z 0, Z2Z 1, Z2Z 1])) (i))) )) ) )])    )  (subBytes_aux (Z256Z (Poly(xs))) (Z256Z(Poly(y))) (i-1) )
+subBytes_aux (Z256Z (Poly(x:xs))) (Z256Z(Poly(y))) (i) =  concatZ256Z  (  Z256Z (Poly [( x `xor` ( ( getEl (Z256Z(Poly(y))) ( (i+4) `mod` (8)) ) `xor` ( ( getEl (Z256Z(Poly(y))) ((i+5) `mod` (8)) ) `xor` ( ( getEl ((Z256Z(Poly(y)))) ( (i+6) `mod` (8))) `xor` ( (getEl ((Z256Z(Poly(y)))) ((i+7) `mod` (8))) `xor` (getEl (Z256Z (Poly [Z2Z 0, Z2Z 1, Z2Z 1, Z2Z 0, Z2Z 0, Z2Z 0, Z2Z 1, Z2Z 1])) (i))) )) ) )])    )  (subBytes_aux (Z256Z (Poly(xs))) (Z256Z(Poly(y))) (i-1) )
