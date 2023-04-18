e1e59a82b2af292f3579efb78f97dadaa353a708
Rgoplay <v.ronan63@gmail.com>
2023-04-18 16:02:54 +0200

    Add concatZ256Z

--- AesFunctions.hs
+++ AesFunctions.hs
@@ -8,4 +8,10 @@ import ZsurNZ
 getEl :: Z_sur_256Z -> Int -> Z_sur_2Z
 getEl (Z256Z (Poly tab)) x = tab!!x
 
+concatZ256Z :: Z_sur_256Z -> Z_sur_256Z -> Z_sur_256Z
+concatZ256Z (Z256Z (Poly a)) (Z256Z (Poly b)) = Z256Z (Poly (a ++ b))
+
+
+
+
 
