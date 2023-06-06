9f48a57fafe1960da12dea806bd1ac8754046930
Ronan Veysseire <ronan.veysseire@grenoble-inp.org>
2023-06-06 20:51:07 +0200

    Optimisation mixColumn en précalculant les multiplications

--- c/src/cbc_aes.c
+++ c/src/cbc_aes.c
@@ -9,32 +9,33 @@
 
 int main(int argc,char *argv[]){
 
-    if (argc != 5) {
+    
+    /*if (argc != 5) {
         printf("Mauvaise entrée !\n");
         printf("Usage : <mode> <path entree> <path sortie> <cle>\n");
         printf("mode : \"encode\", \"decode\" \n");
-        return 0;
-    }
+        return 1;
+    }*/
 
 
-    char input[100] = "";
-    char output[100] = "";
-    byte cle[65] = "";
+    char input[100] = "/home/ronan/Bureau/10M.iso";
+    char output[100] = "/home/ronan/Bureau/10Mcode";
+    byte cle[65] = "aaaaaaaaaaaaaaaa";
 
-    memcpy(input, argv[2], strlen(argv[2]));
+    /*memcpy(input, argv[2], strlen(argv[2]));
     memcpy(output, argv[3], strlen(argv[3]));
-    memcpy(cle, argv[4], strlen(argv[4]));
+    memcpy(cle, argv[4], strlen(argv[4]));*/
 
-
-    if (!strcmp(argv[1], "encode")) {
+    chiffrer_cbc(input, output, cle);
+    /*if (!strcmp(argv[1], "encode")) {
         chiffrer_cbc(input, output, cle);
     }
     if (!strcmp(argv[1], "decode")) {
         dechiffrer_cbc(input, output, cle);
-    }
+    }*/
 
 
-    return 1;
+    return 0;
 }
 
 
