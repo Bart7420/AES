2addf0644eae1d2f5d0d99986da1025e6e781605
etienne <etienne.arnaud-goddet@etu.esisar.grenoble-inp.fr>
2023-06-09 08:31:52 +0200

    correction
    Former-commit-id: ffda03ccecd51d3b02ee9e2d863635553d3e6320

--- c/commandes.sh
+++ c/commandes.sh
@@ -9,15 +9,15 @@ make -B main test=1
 
 
 make -B ecb_aes 
-./ecb_aes encode 100M.iso 100code.ecb 000102030405060708090a0b0c0d0e0f
+./ecb_aes encode 1G.iso 100code.ecb 000102030405060708090a0b0c0d0e0f
 ./ecb_aes decode 100code.ecb 100decode.ecb 000102030405060708090a0b0c0d0e0f
-diff -s 100M.iso 100decode.ecb
+diff -s 1G.iso 100decode.ecb
 
 
 make -B cbc_aes
-./cbc_aes encode 100M.iso 100code.cbc 000102030405060708090a0b0c0d0e0f
+./cbc_aes encode 1G.iso 100code.cbc 000102030405060708090a0b0c0d0e0f
 ./cbc_aes decode 100code.cbc 100decode.cbc 000102030405060708090a0b0c0d0e0f
-diff -s 100M.iso 100decode.cbc
+diff -s 1G.iso 100decode.cbc
 
 make bmp_aes -B
 ./bmp_aes encode ecb entree.bmp bmpcode.ecb 000102030405060708090a0b0c0d0e0f
